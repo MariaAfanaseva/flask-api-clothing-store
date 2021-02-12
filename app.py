eec255e627f9f9fb185fd0869c0fed27a576a262
Maria Afanaseva <mashenkachukina@gmail.com>
2021-02-12 22:48:41 +0100

    Add sqlite db.

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from db import db
+from flask_restful import Api
+from resources.menu_items import MenuItems
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+api.add_resource(MenuItems, "/menu")
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
