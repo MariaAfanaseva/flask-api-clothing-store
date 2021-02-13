0ea94891e7144d06904353c3219e6a421a893720
Maria Afanaseva <mashenkachukina@gmail.com>
2021-02-13 15:07:49 +0100

    Add flask cors.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from dotenv import load_dotenv
+from flask_cors import CORS
 from db import db
 from resources.menu_items import MenuItems
 from fill_db import UpdateDb
@@ -12,7 +13,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 api = Api(app)
-
 load_dotenv('.env')
 
 DEBUG = (os.getenv('DEBUG') == 'True')
@@ -28,6 +28,8 @@ def update_db():
         update = UpdateDb()
         update.recreate_db()
 
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=DEBUG)
