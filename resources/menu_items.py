65068434bf5039944a892f1878c83797dfe32080
Maria Afanaseva <mashenkachukina@gmail.com>
2021-02-14 19:01:54 +0100

    Add resource for products.

--- resources/menu_items.py
+++ resources/menu_items.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
-from models.menu_items import MenuItemsModel
+from models.menu_item import MenuItem
 
 
 class MenuItems(Resource):
     def get(self):
-        return {"menuItems": [item.json() for item in MenuItemsModel.find_all()]}
+        return {"menuItems": [item.json() for item in MenuItem.find_all()]}
