6cdb08430916cba7ca34584f0d5d6a0e2ed3cc0a
Maria Afanaseva <mashenkachukina@gmail.com>
2021-02-25 12:34:19 +0100

    Add create menu item.

--- resources/items.py
+++ resources/items.py
@@ -1,10 +1,34 @@
-from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource, reqparse
 from models.menu_item import MenuItem
+from models.user import User
+
+
+def request_parser(*args):
+    parser = reqparse.RequestParser()
+    for arg in args:
+        parser.add_argument(
+            arg, type=str, required=True, help=f"{arg} field cannot be blank."
+        )
+    return parser
 
 
 class MenuItems(Resource):
     def get(self):
         return {"menuItems": [item.json() for item in MenuItem.find_all()]}
+
+    @jwt_required(refresh=True)
+    def post(self):
+        user_email = get_jwt_identity()
+        if User.find_by_email(user_email).is_admin:
+            data = request_parser('title', 'imageUrl', 'size', 'linkUrl').parse_args()
+            menu_item = MenuItem(**data)
+            try:
+                menu_item.save_to_db()
+            except Exception as e:
+                return {"msg": f"An error occurred while inserting the item.{e}"}, 500
+            return {"msg": "Menu item is created successfully.", "menu_item": menu_item.json()}, 201
+        return {"msg": "Тo access rights to perform this operation."}, 401
 
 
 class Products(Resource):
